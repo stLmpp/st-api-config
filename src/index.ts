061dc608dcbfcf56387f11ca569fd26ee2977f23
stLmpp <gui.stlmpp@gmail.com>
2023-09-24 01:12:10 -0300

    fix: eslint config

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,3 @@
-import { eslintConfig } from './eslint';
-
 export { prettierConfig } from './prettier';
 export { vitestConfig } from './vitest';
 export { tsupConfig } from './tsup';
-
-export default eslintConfig;
--- tsup.config.ts
+++ tsup.config.ts
@@ -1,9 +1,18 @@
 import { defineConfig } from 'tsup';
 import { tsupConfig } from './src/tsup';
-import { writeFile } from 'node:fs/promises';
+import { rename, writeFile, rm } from 'node:fs/promises';
 
 export default defineConfig({
   ...tsupConfig,
+  entry: {
+    index: './src/index.ts',
+    '.eslintrc': './src/eslint.ts',
+  },
+  dts: {
+    entry: {
+      index: './src/index.ts',
+    },
+  },
   format: 'cjs',
   tsconfig: 'tsconfig.json',
   plugins: [
@@ -16,6 +25,18 @@ export default defineConfig({
           './dist/tsconfig.json',
           JSON.stringify(tsconfig, null, 2),
         );
+      },
+    },
+    {
+      name: 'eslint',
+      buildEnd: async () => {
+        await Promise.all([
+          rename('./dist/.eslintrc.js', '.eslintrc.js'),
+          rm('./dist/.eslintrc.js.map', {
+            force: true,
+            recursive: true,
+          }),
+        ]);
       },
     },
   ],
