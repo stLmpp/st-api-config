85f251d6e67cb1eca24c8c62a80c3c90f5365cb9
stLmpp <gui.stlmpp@gmail.com>
2024-10-10 17:34:55 -0300

    remove rules

--- src/eslint.ts
+++ src/eslint.ts
@@ -99,10 +99,7 @@ export const eslint = [
         'error',
         { ignoreRestSiblings: true },
       ],
-      '@typescript-eslint/ban-types': [
-        'error',
-        { types: { object: false }, extendDefaults: true },
-      ],
+      '@typescript-eslint/no-restricted-types': ['error'],
       '@typescript-eslint/naming-convention': [
         'off',
         {
