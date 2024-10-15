874c473593f37d0f77a985986ed443189573c45d
stLmpp <gui.stlmpp@gmail.com>
2024-10-15 16:27:44 -0300

    fix: use experimentalDts generation from tsp

--- src/tsup.ts
+++ src/tsup.ts
@@ -1,11 +1,19 @@
 import type { Options } from 'tsup';
 import fsp from 'node:fs/promises';
+import fs from 'node:fs';
+import timers from 'node:timers/promises';
+
+async function fixDtsFile(filePath: string): Promise<void> {
+  const file = await fsp.readFile(filePath, 'utf-8');
+  await fsp.writeFile(filePath, file.replaceAll(`';`, `.js';`));
+}
 
 export const tsupConfig: Options = {
   entry: ['src/index.ts'],
   sourcemap: true,
   minify: false,
-  dts: true,
+  dts: false,
+  experimentalDts: true,
   format: 'esm',
   platform: 'node',
   tsconfig: 'tsconfig.build.json',
@@ -14,6 +22,23 @@ export const tsupConfig: Options = {
       name: 'clean',
       buildStart: async () => {
         await fsp.rm('dist', { force: true, recursive: true });
+      },
+    },
+    {
+      name: 'dts-fix',
+      buildEnd: () => {
+        (async () => {
+          while (!fs.existsSync('dist/index.d.ts')) {
+            await timers.setTimeout(500);
+          }
+          await fsp.cp('.tsup/declaration/src', 'dist/src', {
+            recursive: true,
+          });
+          await Promise.all([
+            fixDtsFile('dist/index.d.ts'),
+            fixDtsFile('dist/_tsup-dts-rollup.d.ts'),
+          ]);
+        })();
       },
     },
   ],
